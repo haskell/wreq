dbef3514b511c12383744e946b9042e7665a89c6
Bryan O'Sullivan <bos@serpentine.com>
2014-03-28 15:26:25 -0700

    Placeholder implementation of responseHeader

--- Network/WReq/Lens.hs
+++ Network/WReq/Lens.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes, TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Network.WReq.Lens
@@ -15,6 +15,7 @@ module Network.WReq.Lens
     , HTTP.Response
     , responseStatus
     , responseVersion
+    , responseHeader
     , responseHeaders
     , responseBody
     , responseCookieJar
@@ -28,8 +29,14 @@ import Lens.Family.TH (mkLensesBy)
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.WReq.Types as Types
 import qualified Network.HTTP.Types.Status as HTTP
+import Control.Lens
+import Data.ByteString
+import Data.CaseInsensitive
 
 mkLensesBy Just ''Types.Options
 mkLensesBy Just ''HTTP.Proxy
 mkLensesBy Just ''HTTP.Response
 mkLensesBy Just ''HTTP.Status
+
+responseHeader :: CI ByteString -> Traversal' (HTTP.Response body) ByteString
+responseHeader x = responseHeaders . traverse . itraversed . indices (==x)
