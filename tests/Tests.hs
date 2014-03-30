ee05497d27672da7543ce4b99f0ea07cf4d84fc2
Bryan O'Sullivan <bos@serpentine.com>
2014-03-29 20:45:03 -0700

    Refactor to support testing over https

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -9,44 +9,46 @@ import Control.Monad (unless)
 import Data.Aeson (Value(..))
 import Data.Aeson.Lens (key)
 import Data.Maybe (isJust)
+import Data.Monoid ((<>))
 import Network.HTTP.Client (HttpException(..))
 import Network.HTTP.Types.Status (status200)
 import Network.WReq
 import Prelude hiding (head)
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (Assertion, assertBool, assertEqual, assertFailure)
+import Test.HUnit (assertBool, assertEqual, assertFailure)
 import qualified Control.Exception as E
 
-basicGet = do
-  r <- get "http://httpbin.org/get" >>= json
+basicGet site = do
+  r <- get (site "/get") >>= json
   assertEqual "GET succeeds" status200 (r ^. responseStatus)
   let body = r ^. responseBody :: Value
   assertBool "GET request has User-Agent header" $
     isJust (body ^? key "headers" . key "User-Agent")
 
-basicPost = do
-  r <- post "http://httpbin.org/post" (binary "wibble") >>= json
+basicPost site = do
+  r <- post (site "/post") (binary "wibble") >>= json
   let body = r^.responseBody :: Value
   assertEqual "POST succeeds" status200 (r ^. responseStatus)
   assertEqual "POST echoes input" (Just "wibble") (body ^? key "data")
   assertEqual "POST is binary" (Just "application/octet-stream")
                                (body ^? key "headers" . key "Content-Type")
 
-basicHead = do
-  r <- head "http://httpbin.org/get"
+basicHead site = do
+  r <- head (site "/get")
   assertEqual "HEAD succeeds" status200 (r ^. responseStatus)
 
-basicPut = do
-  r <- put "http://httpbin.org/put" (binary "wibble")
+basicPut site = do
+  r <- put (site "/put") (binary "wibble")
   assertEqual "PUT succeeds" status200 (r ^. responseStatus)
 
-basicDelete = do
-  r <- delete "http://httpbin.org/delete"
+basicDelete site = do
+  r <- delete (site "/delete")
   assertEqual "DELETE succeeds" status200 (r ^. responseStatus)
 
-throwsStatusCode = assertThrows "404 causes exception to be thrown" inspect $
-                   head "http://httpbin.org/status/404"
+throwsStatusCode site =
+    assertThrows "404 causes exception to be thrown" inspect $
+    head (site "/status/404")
   where inspect e = case e of
                       StatusCodeException _ _ _ -> return ()
                       _ -> assertFailure "unexpected exception thrown"
@@ -56,15 +58,20 @@ assertThrows desc inspect act = do
   caught <- (act >> return False) `E.catch` \e -> inspect e >> return True
   unless caught (assertFailure desc)
 
-tests = [
+testsWith site = [
     testGroup "basic" [
-      testCase "get" basicGet
-    , testCase "post" basicPost
-    , testCase "head" basicHead
-    , testCase "put" basicPut
-    , testCase "delete" basicDelete
-    , testCase "404" throwsStatusCode
+      testCase "get" $ basicGet site
+    , testCase "post" $ basicPost site
+    , testCase "head" $ basicHead site
+    , testCase "put" $ basicPut site
+    , testCase "delete" $ basicDelete site
+    , testCase "404" $ throwsStatusCode site
     ]
+  ]
+
+tests = [
+    testGroup "http" $ testsWith ("http://httpbin.org" <>)
+  , testGroup "https" $ testsWith ("https://httpbin.org" <>)
   ]
 
 main = defaultMain tests
