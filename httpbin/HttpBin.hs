0894345a0a51ccc5f50d823b87daaeaa0d03b9e5
Bryan O'Sullivan <bos@serpentine.com>
2014-04-01 21:35:58 -0700

    Complete the get portion of httpbin

--- httpbin/HttpBin.hs
+++ httpbin/HttpBin.hs
@@ -4,11 +4,12 @@
 module Main (main) where
 
 import Data.Aeson (encode, object, toJSON)
+import Data.CaseInsensitive (original)
+import Data.Monoid ((<>))
 import Data.Text.Encoding (decodeUtf8)
 import Snap.Core
 import Snap.Http.Server
 import qualified Data.Map as Map
-import Data.CaseInsensitive (original)
 
 get = do
   req <- getRequest
@@ -16,11 +17,17 @@ get = do
       params = Map.foldlWithKey' step Map.empty .
                rqQueryParams $ req
       wibble (k,v) = (decodeUtf8 (original k), decodeUtf8 v)
-      hdrs = Map.fromList . map wibble . listHeaders . headers $ req
-  let obj = object [
-          ("args", toJSON params)
-        , ("headers", toJSON hdrs)
-        ]
+      rqHeaders = headers req
+      hdrs = Map.fromList . map wibble . listHeaders $ rqHeaders
+      url = case getHeader "Host" rqHeaders of
+              Nothing   -> []
+              Just host -> [("url", toJSON . decodeUtf8 $
+                                    "http://" <> host <> rqURI req)]
+  let obj = object $ [
+              ("args", toJSON params)
+            , ("headers", toJSON hdrs)
+            , ("origin", toJSON . decodeUtf8 . rqRemoteAddr $ req)
+            ] <> url
   modifyResponse $ setContentType "application/json"
   writeLBS (encode obj)
 
