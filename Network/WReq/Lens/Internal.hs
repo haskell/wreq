8cd170bcc5902e1e68ef36e4c3e73868033fcc6b
Bryan O'Sullivan <bos@serpentine.com>
2014-04-02 23:24:51 -0700

    Write more idiomatic definitions of assoc and assoc2
    --HG--
    extra : amend_source : 6b53bd830a99792bcf45220e7265a57a38d4d052

--- Network/WReq/Lens/Internal.hs
+++ Network/WReq/Lens/Internal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes, TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Network.WReq.Lens.Internal
@@ -26,21 +26,19 @@ module Network.WReq.Lens.Internal
     , assoc2
     ) where
 
-import Control.Applicative (Applicative(..), (<$>))
-import Control.Lens (_1)
+import Control.Lens hiding (makeLenses)
 import Data.List (partition)
 import Network.WReq.Lens.Machinery (makeLenses)
 import qualified Network.HTTP.Client as HTTP
 
 makeLenses ''HTTP.Request
 
-assoc :: (Eq k, Applicative f) => k -> (a -> f a) -> [(k, a)] -> f [(k, a)]
-assoc n f = go
-  where go []         = pure []
-        go (ab@(a,b):as)
-          | a == n    = ((:).(,) a) <$> f b <*> go as
-          | otherwise = (ab:) <$> go as
+assoc :: (Eq k) => k -> IndexedTraversal' k [(k, a)] a
+assoc i = traverse . itraversed . index i
 
-assoc2 :: (Eq b, Functor f) => b -> ([a] -> f [a]) -> [(b, a)] -> f [(b, a)]
+assoc2 :: Eq k => k -> Lens' [(k,a)] [a]
+-- This is only a lens up to the ordering of the list (which changes
+-- when we modify the list).
+-- assoc2 :: (Eq b, Functor f) => b -> ([a] -> f [a]) -> [(b, a)] -> f [(b, a)]
 assoc2 k f = fmap (uncurry ((++) . fmap ((,) k))) .
              _1 (f . fmap snd) . partition ((==k) . fst)
