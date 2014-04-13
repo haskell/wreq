a3e73f9c87aab389f73c00114c3cbf1ba60d94be
Bryan O'Sullivan <bos@serpentine.com>
2014-04-12 22:01:24 -0700

    Add session post support

--- Network/WReq/Session.hs
+++ Network/WReq/Session.hs
@@ -2,13 +2,25 @@ module Network.WReq.Session
     (
       Session
     , new
+    -- * HTTP verbs
     , get
+    , post
+    -- , head
+    -- , options
+    -- , put
+    -- , delete
+    -- ** Configurable verbs
     , getWith
+    , postWith
+    -- , headWith
+    -- , optionsWith
+    -- , putWith
+    -- , deleteWith
     ) where
 
 import Control.Concurrent.MVar (MVar, modifyMVar, newMVar)
 import Control.Lens
-import Network.WReq (Options, Response)
+import Network.WReq (Options, Payload, Response, defaults)
 import qualified Data.ByteString.Lazy as L
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.WReq as WReq
@@ -26,10 +38,20 @@ new = do
   return Session { seshCookies = ref }
 
 get :: Session -> String -> IO (Response L.ByteString)
-get = getWith WReq.defaults
+get = getWith defaults
+
+post :: Session -> String -> Payload -> IO (Response L.ByteString)
+post = postWith defaults
 
 getWith :: Options -> Session -> String -> IO (Response L.ByteString)
 getWith opts sesh url =
   modifyMVar (seshCookies sesh) $ \cj -> do
     resp <- WReq.getWith (opts & WReq.cookies .~ cj) url
+    return (resp ^. WReq.responseCookieJar, resp)
+
+postWith :: Options -> Session -> String -> Payload
+         -> IO (Response L.ByteString)
+postWith opts sesh url payload =
+  modifyMVar (seshCookies sesh) $ \cj -> do
+    resp <- WReq.postWith (opts & WReq.cookies .~ cj) url payload
     return (resp ^. WReq.responseCookieJar, resp)
