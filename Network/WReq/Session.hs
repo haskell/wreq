84cccd895e1d0056e09dd976dc7b42419848ada9
Bryan O'Sullivan <bos@serpentine.com>
2014-04-12 22:11:52 -0700

    Add session head support

--- Network/WReq/Session.hs
+++ Network/WReq/Session.hs
@@ -5,14 +5,14 @@ module Network.WReq.Session
     -- * HTTP verbs
     , get
     , post
-    -- , head
+    , head
     -- , options
     -- , put
     -- , delete
     -- ** Configurable verbs
     , getWith
     , postWith
-    -- , headWith
+    , headWith
     -- , optionsWith
     -- , putWith
     -- , deleteWith
@@ -21,6 +21,7 @@ module Network.WReq.Session
 import Control.Concurrent.MVar (MVar, modifyMVar, newMVar)
 import Control.Lens
 import Network.WReq (Options, Payload, Response, defaults)
+import Prelude hiding (head)
 import qualified Data.ByteString.Lazy as L
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.WReq as WReq
@@ -43,6 +44,9 @@ get = getWith defaults
 post :: Session -> String -> Payload -> IO (Response L.ByteString)
 post = postWith defaults
 
+head :: Session -> String -> IO (Response ())
+head = headWith defaults
+
 getWith :: Options -> Session -> String -> IO (Response L.ByteString)
 getWith opts sesh url =
   override opts sesh $ \opts' -> WReq.getWith opts' url
@@ -51,6 +55,10 @@ postWith :: Options -> Session -> String -> Payload
          -> IO (Response L.ByteString)
 postWith opts sesh url payload =
   override opts sesh $ \opts' -> WReq.postWith opts' url payload
+
+headWith :: Options -> Session -> String -> IO (Response ())
+headWith opts sesh url =
+  override opts sesh $ \opts' -> WReq.headWith opts' url
 
 override opts sesh act =
   modifyMVar (seshCookies sesh) $ \cj -> do
