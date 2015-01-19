85d0a124f639156f170e65e8d9a4b500d5add5e7
Jasper Van der Jeugt <m@jaspervdj.be>
2015-01-19 18:19:48 +0100

    Use psqueues to simplify and speed up Store

--- Network/Wreq/Cache/Store.hs
+++ Network/Wreq/Cache/Store.hs
@@ -15,8 +15,7 @@ import Data.Hashable (Hashable)
 import Data.Int (Int64)
 import Data.List (foldl')
 import Prelude hiding (lookup, map)
-import qualified Data.HashMap.Lazy as HM
-import qualified Data.PSQueue as PSQ
+import qualified Data.HashPSQ as HashPSQ
 
 type Epoch = Int64
 
@@ -24,8 +23,7 @@ data Store k v = Store {
     capacity :: {-# UNPACK #-} !Int
   , size     :: {-# UNPACK #-} !Int
   , epoch    :: {-# UNPACK #-} !Epoch
-  , lru      :: !(PSQ.PSQ k Epoch)
-  , map      :: !(HM.HashMap k v)
+  , psq      :: !(HashPSQ.HashPSQ k Epoch v)
   }
 
 instance (Show k, Show v, Ord k, Hashable k) => Show (Store k v) where
@@ -34,43 +32,29 @@ instance (Show k, Show v, Ord k, Hashable k) => Show (Store k v) where
 empty :: Ord k => Int -> Store k v
 empty cap
   | cap <= 0  = error "empty: invalid capacity"
-  | otherwise = Store cap 0 0 PSQ.empty HM.empty
+  | otherwise = Store cap 0 0 HashPSQ.empty
 {-# INLINABLE empty #-}
 
 insert :: (Ord k, Hashable k) => k -> v -> Store k v -> Store k v
-insert k v st@Store{..}
-  | size < capacity || present =
-    st { size  = if present then size else size + 1
-       , epoch = epoch + 1
-       , lru   = PSQ.insert k epoch lru
-       , map   = HM.insert k v map
-       }
-  | otherwise =
-      let Just (mink PSQ.:-> _, lru0) = PSQ.minView lru
-      in st { epoch = epoch + 1
-            , lru   = PSQ.insert k epoch lru0
-            , map   = HM.insert k v $ if mink == k
-                                      then map
-                                      else HM.delete mink map
-            }
-  where present = k `HM.member` map
+insert k v st@Store{..} = case HashPSQ.insertView k epoch v psq of
+  (Just (_, _), psq0) -> st {epoch = epoch + 1, psq = psq0}
+  (Nothing,     psq0)
+    | size < capacity -> st {size = size + 1, epoch = epoch + 1, psq = psq0}
+    | otherwise       -> st {epoch = epoch + 1, psq = HashPSQ.deleteMin psq0}
 {-# INLINABLE insert #-}
 
 lookup :: (Ord k, Hashable k) => k -> Store k v -> Maybe (v, Store k v)
-lookup k st@Store{..} = do
-  v <- HM.lookup k map
-  let !st' = st { epoch = epoch + 1, lru = PSQ.insert k epoch lru }
-  return (v, st')
+lookup k st@Store{..} = case HashPSQ.alter tick k psq of
+  (Nothing, _)   -> Nothing
+  (Just v, psq0) -> Just (v, st { epoch = epoch + 1, psq = psq0 })
+  where tick Nothing       = (Nothing, Nothing)
+        tick (Just (_, v)) = (Just v, Just (epoch, v))
 {-# INLINABLE lookup #-}
 
 delete :: (Ord k, Hashable k) => k -> Store k v -> Store k v
-delete k st@Store{..}
-  | k `HM.member` map =
-    st { size = size - 1
-       , lru  = PSQ.delete k lru
-       , map  = HM.delete k map
-       }
-  | otherwise = st
+delete k st@Store{..} = case HashPSQ.deleteView k psq of
+  Nothing           -> st
+  Just (_, _, psq0) -> st {size = size - 1, psq = psq0}
 {-# INLINABLE delete #-}
 
 fromList :: (Ord k, Hashable k) => Int -> [(k, v)] -> Store k v
@@ -78,6 +62,5 @@ fromList = foldl' (flip (uncurry insert)) . empty
 {-# INLINABLE fromList #-}
 
 toList :: (Ord k, Hashable k) => Store k v -> [(k, v)]
-toList Store{..} = [(k,v) | (k PSQ.:-> _) <- PSQ.toList lru,
-                            let v = map HM.! k]
+toList Store{..} = [(k,v) | (k, _, v) <- HashPSQ.toList psq]
 {-# INLINABLE toList #-}
