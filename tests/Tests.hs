11c1124288767e577a8ac7de5c8d8da1a0f6dcdb
Bryan O'Sullivan <bos@serpentine.com>
2015-04-01 18:53:20 -0700

    Try to only build AWS tests if needed

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import Test.Framework (testGroup)
 import UnitTests (testWith)
-import qualified AWS (tests)
 import qualified Properties.Store
+
+#if defined(AWS_TESTS)
+import qualified AWS (tests)
+#endif
 
 main :: IO ()
 main = do
+#if defined(AWS_TESTS)
   awsTests <- AWS.tests
+#else
+  let awsTests = testGroup "aws" []
+#endif
   testWith [
       testGroup "store" Properties.Store.tests
     , awsTests
